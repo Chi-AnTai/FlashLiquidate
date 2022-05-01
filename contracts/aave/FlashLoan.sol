7aaf1a40e3256768b3bdef8638f073907e8be422
Chi-AnTai <andy.chian.tai@gmail.com>
2022-05-01 16:29:40 +0800

    feat: able to flash liquidate

--- contracts/aave/FlashLoan.sol
+++ contracts/aave/FlashLoan.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import { FlashLoanReceiverBase } from "./FlashLoanReceiverBase.sol";
 import { ILendingPool, ILendingPoolAddressesProvider, IERC20 } from "./Interfaces.sol";
@@ -7,20 +8,44 @@ import { SafeMath } from "./Libraries.sol";
 
 import "hardhat/console.sol";
 
+//import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
 
 interface ICToken {
     function liquidateBorrow(address borrower, uint repayAmount, address cTokenCollateral) external returns (uint);
+
+    function redeem(uint redeemTokens) external returns (uint);
 }
 
-/** 
-    !!!
-    Never keep funds permanently on your FlashLoanReceiverBase contract as they could be 
-    exposed to a 'griefing' attack, where the stored funds are used by an attacker.
-    !!!
- */
+interface IWeth {
+    function deposit() external payable;
+}
+
+interface ISwapRouter {
+    struct ExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+        uint160 sqrtPriceLimitX96;
+    }
+
+    function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
+}
+
+
 contract FlashLoan is FlashLoanReceiverBase {
     using SafeMath for uint256;
+
+    address public cETH = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
+    address public cUSDC = 0x39AA39c021dfbaE8faC545936693aC917d5E7563;
+    address public wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public uniswapRouter = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    uint256 public liquidateAmount = 1300000000;
 
     constructor(ILendingPoolAddressesProvider _addressProvider) FlashLoanReceiverBase(_addressProvider) public {}
 
@@ -38,18 +63,31 @@ contract FlashLoan is FlashLoanReceiverBase {
         override
         returns (bool)
     {
-        console.log('executeOperation');
-        IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48).approve(0x39AA39c021dfbaE8faC545936693aC917d5E7563, 1300000000);
-        ICToken(0x39AA39c021dfbaE8faC545936693aC917d5E7563).liquidateBorrow(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 1300000000, 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5);
-        //
-        // This contract now has the funds requested.
-        // Your logic goes here.
-        //
+        // approve and liquidate 
+        IERC20(usdc).approve(cUSDC, liquidateAmount);
+        ICToken(cUSDC).liquidateBorrow(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, liquidateAmount, cETH);
+        
+        // receive cETH after liquidate, redeem it to ETH
+        ICToken(cETH).redeem(IERC20(cETH).balanceOf(address(this)));
 
-        // At the end of your logic above, this contract owes
-        // the flashloaned amounts + premiums.
-        // Therefore ensure your contract has enough to repay
-        // these amounts.
+        // 798797807581625461 is the redeem ETH amount, wrap it to WETH to swap in uniswap
+        IWeth(wETH).deposit{value: 798797807581625461}();
+        uint256 wethBalance = IERC20(wETH).balanceOf(address(this));
+
+        // swap WETH to USDC in uniswap to prepare USDC for repay flashloan
+        IERC20(wETH).approve(uniswapRouter, wethBalance);
+        ISwapRouter.ExactInputSingleParams memory swapParams =
+        ISwapRouter.ExactInputSingleParams({
+            tokenIn: wETH,
+            tokenOut: usdc,
+            fee: 3000,
+            recipient: address(this),
+            deadline: block.timestamp,
+            amountIn: wethBalance,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        ISwapRouter(uniswapRouter).exactInputSingle(swapParams);
 
         // Approve the LendingPool contract allowance to *pull* the owed amount
         for (uint i = 0; i < assets.length; i++) {
@@ -60,16 +98,15 @@ contract FlashLoan is FlashLoanReceiverBase {
         return true;
     }
 
-    function myFlashLoanCall() public {
-        console.log('myFlashLoanCall');
+    function flashLiquidate() public {
         address receiverAddress = address(this);
 
         address[] memory assets = new address[](1);
-        assets[0] = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48); // USDC
+        assets[0] = address(usdc); // USDC
         
 
         uint256[] memory amounts = new uint256[](1);
-        amounts[0] = 100000;
+        amounts[0] = liquidateAmount;
 
         // 0 = no debt, 1 = stable, 2 = variable
         uint256[] memory modes = new uint256[](1);
@@ -89,5 +126,9 @@ contract FlashLoan is FlashLoanReceiverBase {
             params,
             referralCode
         );
+    }
+
+    receive() external payable {
+        
     }
 }
--- scripts/fork-script.js
+++ scripts/fork-script.js
@@ -1,14 +1,15 @@
 
 const hre = require("hardhat");
 const ethers = hre.ethers;
-const { BigNumber } = ethers;
+const { BigNumber, utils } = ethers;
+const { parseEther, formatEther } = utils;
 const { expect } = require('chai');
 const comptrollerABI = require("./ABI.json");
 
 async function main() {
+    // parameters
     const mintAmount = BigNumber.from(10).pow(18)
     const borrowAmount = BigNumber.from(10).pow(6).mul(2600)
-
     const adminAddress = "0x6d903f6003cca6255d85cca4d3b5e5146dc33925";
     const cETHAddress = "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5"
     const comptrollerAddress = '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b'
@@ -16,11 +17,10 @@ async function main() {
     const usdcAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
     const usdcHolderAddress = '0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8';
 
+    // get account and impersonate usdc and comptroller admin
     const accounts = await ethers.getSigners();
-
     const user = accounts[0]
     const liquidator = accounts[1]
-
     await hre.network.provider.request({
         method: 'hardhat_impersonateAccount',
         params: [adminAddress],
@@ -28,7 +28,6 @@ async function main() {
     const adminSigner = await ethers.getSigner(
         adminAddress
     );
-
     await hre.network.provider.request({
         method: 'hardhat_impersonateAccount',
         params: [usdcHolderAddress],
@@ -37,74 +36,36 @@ async function main() {
         usdcHolderAddress
     );
 
+    // prepare contract
     const comptroller =  await ethers.getContractAt(comptrollerABI, comptrollerAddress);
     const cETHContract = await hre.ethers.getContractFactory("CEther");
     const cETH = cETHContract.attach(cETHAddress)
-
     const cTokenContract = await hre.ethers.getContractFactory("CErc20");
     const cToken = cTokenContract.attach(cUsdcAddress);
-
     const usdcContract = await hre.ethers.getContractFactory("CErc20");
     const usdc = usdcContract.attach(usdcAddress);
 
+    // user mint cETH, borrow cUSDC
     await cETH.connect(user).mint({ value: mintAmount })
     await comptroller.connect(user).enterMarkets([cETHAddress, cUsdcAddress])
-
-    let balance = await cETH.balanceOf(user.address)
-    console.log(balance)
-    let userLiquidity = await comptroller.getAccountLiquidity(user.address)
-    console.log(userLiquidity)
-
-
     await cToken.connect(user).borrow(borrowAmount)
-    let usdcBalance = await usdc.balanceOf(user.address)
-    console.log(usdcBalance)
 
-    userLiquidity = await comptroller.getAccountLiquidity(user.address)
-    console.log(userLiquidity)
-
-
+    // decrease collateralfactor so user is in shortfall
     await comptroller.connect(adminSigner)._setCollateralFactor(cETHAddress, BigNumber.from(4).mul(BigNumber.from(10).pow(17)))
+    await comptroller.connect(adminSigner)._setLiquidationIncentive(parseEther('2'))
     
-    userLiquidity = await comptroller.getAccountLiquidity(user.address)
-    console.log(userLiquidity)
-
-    await usdc.connect(usdcSigner).transfer(liquidator.address, borrowAmount)
-    let liquidatorUsdcBalance = await usdc.balanceOf(liquidator.address)
-    console.log('liquidatorUsdcBalance', liquidatorUsdcBalance)
-    await usdc.connect(liquidator).approve(cToken.address, borrowAmount);
-
-    let balanceBeforeLiquidate = await cETH.balanceOf(user.address)
-    // await cToken.connect(liquidator).liquidateBorrow(user.address, borrowAmount.div(2), cETH.address)
-    let balanceAfterLiquidate = await cETH.balanceOf(user.address)
-
-    userLiquidity = await comptroller.getAccountLiquidity(user.address)
-    console.log(userLiquidity)
-
-    console.log('balanceBeforeLiquidate', balanceBeforeLiquidate)
-    console.log('balanceAfterLiquidate', balanceAfterLiquidate)
-
-    console.log('user', user.address)
-    console.log('cToken', cToken.address)
-
-    // await deplpyFlashLoan()
+    // deploy flashLoan contract
     const flashLoanContract =  await hre.ethers.getContractFactory("FlashLoan");
     const flashLoan = await flashLoanContract.deploy('0xb53c1a33016b2dc2ff3653530bff1848a515c8c5')
-    console.log('deploy success',flashLoan.address )
+    console.log('flashLoan deploy success',flashLoan.address )
 
+    // call falshLiquidate
     await usdc.connect(usdcSigner).transfer(flashLoan.address, borrowAmount)
-    await flashLoan.myFlashLoanCall()
+    await flashLoan.flashLiquidate()
 
-    const ethBalance = await ethers.provider.getBalance(flashLoan.address)
-    console.log('ethBalance', ethBalance)
-    const cETHBalance = await cETH.balanceOf(flashLoan.address)
-    console.log('cETHBalance', cETHBalance)
-
-
-
-
-
-
+    // usdc balance increase due to flash liquidate
+    let usdcBalance = await usdc.balanceOf(flashLoan.address)
+    console.log('flash load profit:  usdc', usdcBalance)
 
 }
 
